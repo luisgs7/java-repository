00c3465ce2a0e12d386636b338af2ae92f6de2c4
luisgs7 <luisgsilva270@gmail.com>
2021-06-11 17:02:47 -0300

    new class Funcionario

--- src/Funcionario.java
+++ src/Funcionario.java
@@ -1,2 +1,9 @@
 public class Funcionario {
+    String nome;
+    double salario;
+
+    public double recuperarSalario( double bonus, double desconto){
+        this.salario = this.salario - desconto;
+        return this.salario + bonus;
+    }
 }
